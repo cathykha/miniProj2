a38a4366ade160f8c7617d7a82cc94b685e846b9
cathykh <pk552@njit.edu>
2018-10-23 14:49:09 -0400

    Created Database Seed - 50 Cars

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -6,7 +6,8 @@ $factory->define(App\Car::class, function (Faker $faker) {
 
     return [
         'make'=>$faker->randomElement($array=array('ford', 'honda', 'toyota')),
-
+        'model'=>$faker->regexify('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'),
+        'year'=>$faker->year(),
 
     ];
 });
